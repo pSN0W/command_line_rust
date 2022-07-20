d47af43c6d31933605e38b574e379e3ddb23190c
pSN0W <08yaksh08@gmail.com>
2022-07-20 18:45:27 +0530

    Completed code

--- rusty_echo/src/main.rs
+++ rusty_echo/src/main.rs
@@ -1,3 +1,32 @@
+use clap::{App,Arg};
 fn main() {
-    println!("{:?}",std::env::args());
+    let matches = App::new("rusty_echo")
+                            .version("0.1.0")
+                            .author("Pratyaksh Singh <09yaksh09@gmail.com>")
+                            .about("Implementation of echo in rust")
+                            .arg(
+                                Arg::with_name("text")
+                                    .value_name("TEXT")
+                                    .help("Input a text to display")
+                                    .required(true)
+                                    .min_values(1)
+                            )
+                            .arg(
+                                Arg::with_name("omit_newline")
+                                    .short("n")
+                                    .help("Do not print new line")
+                                    .takes_value(false)
+                            )
+                            .get_matches();
+
+    let text = match matches.values_of_lossy("text") {
+        Some(x) => x,
+        None => unreachable!("clap should have thrown error")
+    };
+    let include_break = matches.is_present("omit_newline");
+    let mut final_output = text.join(" ");
+    if !include_break {
+        final_output += "\n";
+    }
+    print!("{}",final_output);
 }
